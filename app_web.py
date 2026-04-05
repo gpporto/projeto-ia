b9bdd760cb0dc42cd0b5270dd02962b3593c504e
gpporto_profile <156333537+gpporto@users.noreply.github.com>
2026-04-05 17:12:35 -0300

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -9,7 +9,7 @@ import os
 #client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 st.set_page_config(page_title="Chat com PDF", layout="wide")
 
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="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")
 
 st.title("📄 Chat com PDF (IA)")
 st.write("Faça upload de um PDF e converse com ele")
