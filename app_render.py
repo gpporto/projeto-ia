ab8d6f08b1ef1df84728efc8dc549f435d2c8fad
guilherme <guilherme.pporto@gmail.com>
2026-04-08 21:23:32 -0300

    clean 18

--- app_render.py
+++ app_render.py
@@ -7,6 +7,9 @@ import os
 
 from supabase import create_client
 
+from dotenv import load_dotenv
+load_dotenv()
+
 supabase = create_client(
     os.getenv("SUPABASE_URL"),
     os.getenv("SUPABASE_KEY")
